c5edf92bae179fca5430de70dbf683aaddf91faa
Cameron Sharp <cameron.royal.sharp@gmail.com>
2018-06-04 18:09:25 -0600

    Fun adding components

---
+++ src/app/app.component.topmenu.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'TopMenu',
+    templateUrl: 'app.component.topmenu.html',
+    styleUrls: ['./app.component.topmenu.css']
+})
+export class TopMenu {
+    title: 'Top Menu';
+}
