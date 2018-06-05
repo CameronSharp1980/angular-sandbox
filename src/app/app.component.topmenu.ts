65c190807c502cfd8562cc3d1ddd1e6301b228af
Cameron Sharp <cameron.royal.sharp@gmail.com>
2018-06-04 20:19:40 -0600

    Fun with Data Binding (ngModel incomplete)

--- src/app/app.component.topmenu.ts
+++ src/app/app.component.topmenu.ts
@@ -7,4 +7,32 @@ import { Component } from "@angular/core";
 })
 export class TopMenu {
     title: 'Top Menu';
+    dataBinding: string = 'Data Binding';
+    eventBinding: string = 'Event Binding';
+    clicked: boolean = false;
+    clickyBool: boolean = false;
+
+    _clickyClicky: string = 'Clickable';
+    get clickyClicky(){
+        return this._clickyClicky;
+    }
+    set clickyClicky(value: string){
+        if(this.clickyBool){
+            this._clickyClicky = 'Clicked!';
+            this.clickyBool = !this.clickyBool;
+        } else{
+            this.clickyBool = !this.clickyBool;
+            this._clickyClicky = 'Clickable';
+        }
+    }
+
+    clickMe(){
+        if(this.clicked){
+            this.clicked = !this.clicked;
+            this.eventBinding = 'True';
+        } else {
+            this.clicked = !this.clicked;
+            this.eventBinding = 'False';
+        }
+    }
 }
